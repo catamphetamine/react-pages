bfed5315137ec538a87c8e7b40470a3cd20d7337
Nikolay <kuchumovn@gmail.com>
2016-11-04 02:27:01 +0300

    hotfix undefined

--- source/page-server/monitoring.js
+++ source/page-server/monitoring.js
@@ -1,7 +1,7 @@
 // https://codeascraft.com/2011/02/15/measure-anything-measure-everything/
 import StatsD from 'lynx'
 
-export default function(settings)
+export default function(settings = {})
 {
 	let statsd
 
