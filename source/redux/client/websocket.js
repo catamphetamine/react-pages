437ad19c07034838ffa2de31b2618cd907d7b820
Nikolay <kuchumovn@gmail.com>
2017-01-25 01:35:08 +0300

    Better websocket reconnect

--- source/redux/client/websocket.js
+++ source/redux/client/websocket.js
@@ -8,9 +8,37 @@ export default function set_up_websocket_connection(settings)
 	{
 		const websocket = new WebSocket(`${settings.secure ? 'wss' : 'ws'}://${settings.host}:${settings.port}`, undefined,
 		{
-			// Exponential backoff, but no less that once in a minute
+			// If a number returned then it's gonna be a delay
+			// before reconnect is attempted.
+			// If anything else is returned (`null`, `undefined`, `false`)
+			// then it means "don't try to reconnect".
+			//
+			// `event` is either a `CloseEvent`
+			// or an online/offline `navigator` event.
+			//
 			shouldReconnect(event, websocket)
 			{
+				// https://github.com/appuri/robust-websocket/issues/8
+				//
+				// 1011 (500) is not retried by the default shouldReconnect.
+				// a 500 will usually either be a big in the server
+				// that the code is hitting, and retrying won't help,
+				// or the server is down and getting slammed,
+				// and retrying will just slam it more.
+				// Sure the server will probably eventually come back up
+				// so retrying it at a longer interval would be fine.
+				//
+				// 1008 (400) means the request you formed is bad.
+				// If you try the exact same request again, you should always get 400.
+				// 400 is not a transient error. If it is, the API is using the wrong status code.
+				//
+				if (event.code === 1008 || event.code === 1011)
+				{
+					// Retry in 15-30 minutes
+					return (15 + Math.random() * 15) * 60 * 1000
+				}
+
+				// Exponential backoff, but no less that once in a minute
 				return Math.min(Math.pow(1.5, websocket.attempts) * 500, 60 * 1000)
 			}
 		})
