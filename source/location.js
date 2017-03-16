8864b3091bbb937797d10dc32ea9d9d82d8176cc
Nikolay <kuchumovn@gmail.com>
2017-03-17 00:01:20 +0300

    Writing my own `parse_location` since `history`'s one is buggy

--- source/location.js
+++ source/location.js
@@ -2,8 +2,6 @@
 // import querystring from 'query-string'
 // import deep_equal from 'deep-equal'
 
-import { parsePath } from 'history/lib/PathUtils'
-
 export function location_url(location)
 {
 	if (typeof location === 'string')
@@ -24,12 +22,31 @@ export function location_url(location)
 	return `${location.pathname}${search}${hash}`
 }
 
+// Doesn't construct `query` though
 export function parse_location(location)
 {
-	if (typeof location === 'string')
+	if (typeof location !== 'string')
 	{
-		return parsePath(location)
+		return location
 	}
 
-	return location
+	let pathname = location
+	let search
+	let hash
+
+	const search_index = pathname.indexOf('?')
+	if (search_index >= 0)
+	{
+		pathname = pathname.slice(0, search_index)
+		search   = pathname.slice(search_index)
+	}
+
+	const hash_index = search.indexOf('#')
+	if (hash_index >= 0)
+	{
+		search = search.slice(0, hash_index)
+		hash   = search.slice(hash_index)
+	}
+
+	return { pathname, search, hash }
 }
